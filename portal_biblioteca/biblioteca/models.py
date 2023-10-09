5acc916a4424d27ae7eee5426b0895af12cc3a5a
rock051 <fernando.roque@estudante.ufla.br>
2023-10-09 19:34:30 -0300

    Criando nosso Primeiro Modelo

--- portal_biblioteca/biblioteca/models.py
+++ portal_biblioteca/biblioteca/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    nome = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    ano = models.IntegerField()
