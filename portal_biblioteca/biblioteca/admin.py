3778365902fbe9fa89e639291dfa508ca1a0a4ce
rock051 <fernando.roque@estudante.ufla.br>
2023-10-09 20:09:01 -0300

    Criando um Usuário no Django

--- portal_biblioteca/biblioteca/admin.py
+++ portal_biblioteca/biblioteca/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Livro
 
-# Register your models here.
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ("nome", "autor", "ano")
+
+admin.site.register(Livro, LivroAdmin)
--- portal_biblioteca/biblioteca/models.py
+++ portal_biblioteca/biblioteca/models.py
@@ -4,3 +4,6 @@ class Livro(models.Model):
     nome = models.CharField(max_length=255)
     autor = models.CharField(max_length=255)
     ano = models.IntegerField()
+
+#def __str__(self):  #definição de função adionada
+ #       return f"{self.nome} - {self.autor}" 
