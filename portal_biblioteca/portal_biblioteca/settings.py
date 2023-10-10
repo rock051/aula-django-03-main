ed665d95c1400fd99bafb02d2fb322af02113fa3
rock051 <fernando.roque@estudante.ufla.br>
2023-10-10 10:04:24 -0300

    Adicionando Controle de Usuários no Django

--- portal_biblioteca/portal_biblioteca/settings.py
+++ portal_biblioteca/portal_biblioteca/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'biblioteca',
+    'usuarios', #adicone seu app aqui 
 ]
 
 MIDDLEWARE = [
--- portal_biblioteca/portal_biblioteca/urls.py
+++ portal_biblioteca/portal_biblioteca/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('biblioteca.urls')),
+    path('auth/', include('usuarios.urls')),   #adicione essa linha aqui
     path('admin/', admin.site.urls),
 ]
