fcfa129e1442db8133ef311d3f85653d97b8cf54
rock051 <fernando.roque@estudante.ufla.br>
2023-10-09 21:37:43 -0300

    from .models import Livro    # adicione esta importação

--- portal_biblioteca/biblioteca/views.py
+++ portal_biblioteca/biblioteca/views.py
@@ -1,65 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Livro    # adicione esta importação
 
 def principal(request):
     template = loader.get_template('principal.html')
     return HttpResponse(template.render())
 
-def livros(request):
+def livros(request):         # atualize esta função
+    livros = Livro.objects.all().values()
     template = loader.get_template('livros.html')
     context = {
-        'livros': [
-            {
-                "nome": "O Senhor dos Anéis",
-                "autor": "J.R.R. Tolkien",
-                "ano": 1954
-            },
-            {
-                "nome": "1984",
-                "autor": "George Orwell",
-                "ano": 1949
-            },
-            {
-                "nome": "Dom Quixote",
-                "autor": "Miguel de Cervantes",
-                "ano": 1605
-            },
-            {
-                "nome": "Cem Anos de Solidão",
-                "autor": "Gabriel García Márquez",
-                "ano": 1967
-            },
-            {
-                "nome": "Harry Potter e a Pedra Filosofal",
-                "autor": "J.K. Rowling",
-                "ano": 1997
-            },
-            {
-                "nome": "Crime e Castigo",
-                "autor": "Fiódor Dostoiévski",
-                "ano": 1866
-            },
-            {
-                "nome": "A Metamorfose",
-                "autor": "Franz Kafka",
-                "ano": 1915
-            },
-            {
-                "nome": "O Grande Gatsby",
-                "autor": "F. Scott Fitzgerald",
-                "ano": 1925
-            },
-            {
-                "nome": "Orgulho e Preconceito",
-                "autor": "Jane Austen",
-                "ano": 1813
-            },
-            {
-                "nome": "Os Miseráveis",
-                "autor": "Victor Hugo",
-                "ano": 1862
-            }
-        ]
+        'livros': livros,
     }
     return HttpResponse(template.render(context, request))
 
